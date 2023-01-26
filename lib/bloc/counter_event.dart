4636e69b0dd81fdda24540f69fa5d0e2f3d4d8b4
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-26 23:58:01 +0300

    Created counter event file

---
+++ lib/bloc/counter_event.dart
@@ -0,0 +1,17 @@
+//Define the events
+import 'package:flutter/foundation.dart';
+
+@immutable
+abstract class CounterEvent {
+  final String value;
+
+  const CounterEvent(this.value);
+}
+
+class IncrementEvent extends CounterEvent {
+  const IncrementEvent(String value) : super(value);
+}
+
+class DecrementEvent extends CounterEvent {
+  const DecrementEvent(String value) : super(value);
+}
